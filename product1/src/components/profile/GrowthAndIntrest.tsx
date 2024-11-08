f45da764713d3f86952cc9fdd6fb7264470fafa1
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-11-08 18:52:06 +0530

    Added ui for growth page

--- product1/src/components/profile/GrowthAndIntrest.tsx
+++ product1/src/components/profile/GrowthAndIntrest.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { LineCharts } from '../shadcn/LineChart'
 
 const GrowthAndIntrest = () => {
-  return (
-    <div>GrowthAndIntrest</div>
-  )
+    return (
+        <div>
+            <div>
+                <h1 className='font-semibold text-2xl'>Growth & interests</h1>
+            </div>
+            <div className='flex justify-around'>
+                <div>
+                    <LineCharts />
+                </div>
+                <div>
+                    <LineCharts />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default GrowthAndIntrest
